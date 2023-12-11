fedda28d3e6bf022037e737f2ffe89dad947fbf6
beambautista <84446151+beambautista@users.noreply.github.com>
2023-12-11 12:10:41 -0600

    Added nutrition facts to recipe page

--- src/pages/Recipe/Details/Details.jsx
+++ src/pages/Recipe/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import dot from '../../../assets/icons/dot.svg'
 import PropTypes from 'prop-types'
+import NutritionFacts from '../../../components/NutritionFacts/NutritionFacts'
 
 function Details({
   nutrients,
@@ -8,6 +9,24 @@ function Details({
   caloricBreakdown,
   weightPerServing,
 }) {
+
+  const nutritionFacts = {
+    calories: { amount: [nutrients?.[0].amount] },
+    fat: { amount: [nutrients?.[1].amount], percentOfDailyNeeds: [nutrients?.[1].percentOfDailyNeeds]},
+    satFat: { amount: [nutrients?.[2].amount], percentOfDailyNeeds: [nutrients?.[2].percentOfDailyNeeds] },
+    transFat: { amount: [], percentOfDailyNeeds: [] },
+    chol: { amount: [nutrients?.[6].amount], percentOfDailyNeeds: [nutrients?.[6].percentOfDailyNeeds] },
+    sodium: { amount: [nutrients?.[7].amount], percentOfDailyNeeds: [nutrients?.[7].percentOfDailyNeeds] },
+    carb: { amount: [nutrients?.[4].amount], percentOfDailyNeeds: [nutrients?.[4].percentOfDailyNeeds] },
+    fiber: { amount: [nutrients?.[21].amount], percentOfDailyNeeds: [nutrients?.[21].percentOfDailyNeeds] },
+    sugar: { amount: [nutrients?.[5].amount], percentOfDailyNeeds: [nutrients?.[5].percentOfDailyNeeds] },
+    protein: { amount: [nutrients?.[9].amount], percentOfDailyNeeds: [nutrients?.[9].percentOfDailyNeeds] },
+    vitD: { amount: [nutrients?.[25].amount], percentOfDailyNeeds: [nutrients?.[25].percentOfDailyNeeds] },
+    calcium: { amount: [nutrients?.[20].amount], percentOfDailyNeeds: [nutrients?.[20].percentOfDailyNeeds] },
+    iron: { amount: [nutrients?.[17].amount], percentOfDailyNeeds: [nutrients?.[17].percentOfDailyNeeds] },
+    potassium: { amount: [nutrients?.[27].amount], percentOfDailyNeeds: [nutrients?.[27].percentOfDailyNeeds] }
+  }
+
   console.log(nutrients)
   console.log(ingredients)
   console.log(caloricBreakdown)
@@ -16,13 +35,13 @@ function Details({
   return (
     <div className="flex w-full mt-10">
       {/* Ingredients */}
-      <div className="bg-background-alt px-8 py-10 w-1/3 min-h-[30rem]">
-        <h2 className="text-5xl font-bold text-header">Ingredients</h2>
-        <div className="mt-10 flex flex-col">
+      <div className="bg-header px-8 py-10 w-1/3 min-h-[30rem] text-background-alt">
+        <h2 className="text-5xl font-bold text-background-alt">Ingredients</h2>
+        <div className="mt-10 flex flex-col text-background-alt">
           {ingredients?.map((item, index) => (
             <div key={index} className="flex items-start">
               <img src={dot} className="w-8 h-8 inline leading-10 mt-1" />
-              <p className="inline text-header text-xl leading-10">
+              <p className="inline text-background-alt text-xl leading-10">
                 {item.amount} {item.unit} {item.name}
               </p>
             </div>
@@ -31,13 +50,9 @@ function Details({
       </div>
 
       {/* Macro Information */}
-      <div className="flex-1 ml-12 bg-header p-12 w-1/4 min-h-[30rem] text-background-alt">
-        <h2 className="text-5xl font-bold text-background-alt">
-          Macro Information
-        </h2>
-
+    
         {/* Macros */}
-        <div className="flex items-center justify-between h-full items-center">
+        {/* <div className="flex items-center justify-between h-full items-center">
           <div>
             <h3 className="text-4xl font-bold leading-[4rem]">
               Calories {nutrients?.[0].amount}
@@ -55,42 +70,16 @@ function Details({
               Carbs {nutrients?.[3].amount}
               {nutrients?.[3].unit}
             </h4>
-          </div>
+          </div> */}
 
           {/* Others */}
           <div className="flex">
-            <div className="flex flex-col text-2xl font-bold">
-              <div>Cholesterol</div>
-              <div>Sodium</div>
-              <div>Calcium</div>
-              <div>Magnesium</div>
-              <div>Potassium</div>
-              <div>Iron</div>
-            </div>
             <div className="ml-20 flex flex-col text-2xl">
-              <div>
-                {nutrients?.[6].amount} {nutrients?.[6].unit}
-              </div>
-              <div>
-                {nutrients?.[7].amount} {nutrients?.[7].unit}
-              </div>
-              <div>
-                {nutrients?.[20].amount} {nutrients?.[20].unit}
-              </div>
-              <div>
-                {nutrients?.[13].amount} {nutrients?.[13].unit}
-              </div>
-              <div>
-                {nutrients?.[15].amount} {nutrients?.[15].unit}
-              </div>
-              <div>
-                {nutrients?.[14].amount} {nutrients?.[14].unit}
-              </div>
+              <NutritionFacts nutritionData={nutritionFacts}/>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+    // </div>
   )
 }
 Details.propTypes = {
