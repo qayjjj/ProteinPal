975296e11debf5e1d6c5c8a18fe30b8858491452
beambautista <84446151+beambautista@users.noreply.github.com>
2023-12-18 23:24:33 +0800

    Fixed footer

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
   <div className="bg-background w-screen h-screen m-auto justify-center">
     <Navigation/>
 
-    <h1 className="text-center text-xl sm:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl 3xl:text-6xl font-bold text-header mt-10 lg:mt-16">
+    <h1 className="text-center text-xl sm:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl 3xl:text-6xl font-bold text-header mt-8 sm:mt-10 lg:mt-16">
       About
     </h1>
 
@@ -44,9 +44,7 @@ export default function About() {
         </div>
     </section>
 
-    <div className="bottom-0 mt-10 w-full">   
     <Footer/>
-    </div> 
   </div>
 
 )}
